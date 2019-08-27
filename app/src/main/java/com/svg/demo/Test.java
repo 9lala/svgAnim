c63a369d74b65167e7b9502c780711a4cf1c8a4c
zaigui9lala <1005089574@qq.com>
2019-08-27 17:51:57 +0800

    init

--- app/src/main/java/com/svg/demo/Test.java
+++ app/src/main/java/com/svg/demo/Test.java
@@ -135,6 +135,7 @@ public class Test {
             for (int j = 0; j < a.length - 1 - i; j++) {
                 if (a[j] > a[j + 1]) {
                     int temp = a[j];
+
                     a[j] = a[j + 1];
                     a[j + 1] = temp;
                 }
